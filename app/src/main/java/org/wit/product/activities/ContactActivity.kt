bd85eaf9773de9e30ab4812d45efc862a744fd83
michaelahealy9 <michaela.healy84@gmail.com>
2019-03-28 13:28:14 +0000

    updating project

--- app/src/main/java/org/wit/product/activities/ContactActivity.kt
+++ app/src/main/java/org/wit/product/activities/ContactActivity.kt
@@ -9,5 +9,8 @@ class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+
     }
+
 }
